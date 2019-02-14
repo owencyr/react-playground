513eede0ec3b36abdedaf6b57c4f5dd94cb83f9c
owencyr <cyroc@dukes.jmu.edu>
2019-02-13 22:21:05 -0500

    form submission and validation

--- src/lang-context/GreatGrandChild.js
+++ src/lang-context/GreatGrandChild.js
@@ -10,6 +10,14 @@ class GreatGrandChild extends Component {
 			<section>
 				<h2>{copy.title}</h2>
 				<p>{copy.body}</p>
+				<button
+					disabled={this.context.lang === "klingon"}
+					onClick={() => this.context.setLang("klingon")}>
+					Klingon!{" "}
+					<span role="img" aria-label="klingon">
+						🖖
+					</span>
+				</button>
 			</section>
 		);
 	}
